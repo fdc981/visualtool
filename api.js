5e76d57d7eccb68d5b33384ed44f013c667dc73b
a1766074 <a1766074@student.adelaide.edu.au>
2021-01-14 13:48:28 +1030

    changed input needed for `make_transform`, added rough JSON parsing

--- api.js
+++ api.js
@@ -1,17 +1,31 @@
 /**
- * Creates a transformation.
- * Example: make_transform({a : window.prev.a + 1}).
+ * Creates a transformation, using a JSON string.
  */
 function make_transform(defn) {
+    if (typeof(defn) !== "string") {
+        return undefined;
+    }
+    
+    var resultobj = {};
+    
+    defn = defn.replace(/[\x20\x0A\x0D\x09]*/g, "");   // replace all whitespace with nothing
+    defn = defn.substring(1, defn.length - 1);         // remove start and end braces
+    defn = defn.split(',');
+    
+    for (let pair of defn) {
+        pair = pair.split(":", 2);
+        let key = pair[0];
+        let val = pair[1];
+
+        resultobj[key] = val;
+    }
+
     return function (x) {
-        window.prev = x;
-        console.log(window.prev);
-        for (var key in defn) {
-            x[key] = defn[key];
+        console.log(resultobj);
+        for (var key in resultobj) {
+            x[key] = eval(resultobj[key].replace("prev", x[key]));
         }
         return x;
     };
 }
 
-// placeholder for previous object
-var prev = {};
