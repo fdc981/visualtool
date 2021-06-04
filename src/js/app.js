fee792ef6343f306f9354010a79b7d17a762e26a
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-06-05 00:35:40 +0930

    implemented repositioning of shapes through holding ctrl then dragging a visible shape

--- src/js/app.js
+++ src/js/app.js
@@ -3,23 +3,40 @@ const app = {
 
     data() {
         return {
-            testList : [
-                "asdf",
-                "ghjkl",
-                "qwerty"
-            ],
-
-            shapeList : []
+            shapeList : [],
+            following : false
         }
     },
 
     methods : {
         showShapeList() {
             console.log(this.shapeList);
+        },
+
+        followMouse(e) {
+            let target = e.target;
+
+            let onMouseMove = (e) => {
+                target.style.left = e.clientX / window.innerWidth * 100 + "%";
+                target.style.top = e.clientY / window.innerHeight * 100 + "%";
+            };
+            document.addEventListener('mousemove', onMouseMove);
+
+            document.addEventListener('mouseup', (e) => {
+                document.removeEventListener('mousemove', onMouseMove);
+            }, { once: true });
+
+        },
+
+        updatePos(e) {
+            this.following = false;
+
+            e.target.style.left = e.clientX / window.innerWidth + "%";
+            e.target.style.top = e.clientY / window.innerHeight + "%";
         }
     },
 
-    components: {
+    components : {
         "add-shape-button": addShapeButton,
         "add-animation-button": addAnimationButton,
         "delete-button": deleteButton,
