a01b28f2b2d87d6781eb5dc80765ad0215500a82
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-06-11 21:59:56 +0930

    changed styling of 'update' and 'finish editing' buttons

--- src/js/edit-area.js
+++ src/js/edit-area.js
@@ -7,8 +7,10 @@ const editArea = {
         <div style="position: absolute; left:50%; top:50%">
             <div id="edit-region" class="bg-white font-monospace p-3" contentEditable="true"
                  v-html="shape.style.replace(/;/g, ';<br>')"></div>
-            <button @click="updateStyle">update</button>
-            <button @click="stopEditing">finish editing</button>
+            <div class="px-3">
+                <a href="#" @click="updateStyle">update</a>&nbsp;
+                <a href="#" @click="stopEditing">finish editing</a>
+            </div>
         </div>
     `,
 
