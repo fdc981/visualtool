176316771058c68f5e381fb802c4ece2b147b894
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-06-24 23:42:27 +0930

    Add ability to tweak numerical parameters in EditArea
    By selecting a number in the editing area and holding Shift, the selected number can then be changed by moving the cursor. The numbers that can be selected are the strings which can be converted to a `Number`.
    
    Releasing Shift stops the changing of the selected number, and updates the shape's style property.

--- src/components/EditArea.vue
+++ src/components/EditArea.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="font-monospace" style="position: absolute; left:50%; top:50%" @mousedown.ctrl.exact="followMouse">
+    <div class="font-monospace"
+         style="position: absolute; left:50%; top:50%"
+         @mousedown.ctrl.exact="followMouse"
+         @keydown.shift="varySelection">
         <div id="edit-region" class="bg-white p-3" contentEditable="true"
              v-html="shape.style.replace(/;/g, ';<br>')"></div>
         <div class="px-3">
@@ -38,6 +41,41 @@ export default {
         stopEditing() {
             this.updateStyle();
             this.shape.currentlyEditing = false;
+        },
+
+        varySelection(e) {
+            let selectedRange = window.getSelection().getRangeAt(0);
+
+            // return if selection is not a number
+            const selection = window.getSelection().toString();
+            if (isNaN(Number(selection)) || selection === "") {
+                return;
+            }
+
+            let originX = Number(window.innerWidth / 2);
+            let originY = Number(window.innerHeight / 2);
+
+            let onmousemove = (e) => {
+                let diff = (e.clientX - originX) + (e.clientY - originY);
+                let newContents = document.createTextNode(String(diff));
+
+                selectedRange.deleteContents();
+                selectedRange.insertNode(newContents);
+            };
+
+            // initial event to set origin, continues to onmousemove
+            document.addEventListener("mousemove", (e) => {
+                originX = e.clientX - Number(selection);
+                originY = e.clientY;
+
+                document.addEventListener("mousemove", onmousemove);
+            }, { once : true });
+
+            document.addEventListener("keyup", () => {
+                document.removeEventListener("mousemove", onmousemove);
+
+                this.updateStyle();
+            }, { once : true });
         }
     }
 }
