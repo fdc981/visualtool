1f4f635c9ff4ea63f6c5032fb675e4a034c43cfe
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-07-09 21:16:44 +0930

    Move `followMouse()` from App to ShapeRender
    `followMouse()` is called on click of the `ShapeRender`, but this method is not present in that component. This commit fixes that.

--- src/App.vue
+++ src/App.vue
@@ -124,25 +124,6 @@
              console.log(this.shapeList);
          },
 
-         followMouse(e) {
-             let target = e.target;
-
-             let onMouseMove = (e) => {
-                 target.style.left = e.clientX / window.innerWidth * 100 + "%";
-                 target.style.top = e.clientY / window.innerHeight * 100 + "%";
-             };
-
-             document.addEventListener('mousemove', onMouseMove);
-
-             document.addEventListener('mouseup', (e) => {
-                 document.removeEventListener('mousemove', onMouseMove);
-
-                 let index = target.attributes.index.value;
-
-                 this.shapeList[index].style = target.style.cssText;
-             }, { once: true });
-         },
-
          addShape() {
              this.shapeList.push({
                  id : this.currID,
--- src/components/ShapeRender.vue
+++ src/components/ShapeRender.vue
@@ -10,6 +10,27 @@
  export default {
      props: {
          shapeIndex: Number
+     },
+
+     methods: {
+         followMouse(e) {
+             let target = e.target;
+
+             let onMouseMove = (e) => {
+                 target.style.left = e.clientX / window.innerWidth * 100 + "%";
+                 target.style.top = e.clientY / window.innerHeight * 100 + "%";
+             };
+
+             document.addEventListener('mousemove', onMouseMove);
+
+             document.addEventListener('mouseup', (e) => {
+                 document.removeEventListener('mousemove', onMouseMove);
+
+                 let index = target.attributes.index.value;
+
+                 this.$parent.shapeList[index].style = target.style.cssText;
+             }, { once: true });
+         },
      }
  }
 </script>
