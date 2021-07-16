956c77508e4702eab83e687c6c83eda09aea6fba
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-07-16 21:08:44 +0930

    Add test for adding shapes via context menu
    The new test added in this commit checks whether a new shape can be added by right-clicking on an empty spot in the viewport to open the context menu, then clicking 'add shape'.

--- tests/frontend.js
+++ tests/frontend.js
@@ -16,5 +16,26 @@ module.exports = {
       .assert.cssProperty("#display div[index='0']", "height", "50px")
       .assert.cssProperty("#display div[index='0']", "background-color", "rgba(0, 0, 0, 0.5)")
       .end();
-  }
+  },
+
+  "New shape can be created via 'add shape' button of context menu" : function(browser) {
+    browser
+      .url('http://localhost:8080/')
+      .assert.titleContains('visualtool')
+      .waitForElementVisible('#display')
+      .moveToElement("#display", 50, 50) // works for chromedriver but not geckodriver?
+      .mouseButtonClick("right")
+      .waitForElementVisible('#context-menu')
+      /* select add shape and click */
+      .useXpath()
+      .click("//a[normalize-space()='add shape']")
+      .useCss()
+      .assert.visible("#display div[index='0']")
+      /* verify that the new shape has appropriate styling */
+      .assert.cssProperty("#display div[index='0']", "position", "absolute")
+      .assert.cssProperty("#display div[index='0']", "width", "50px")
+      .assert.cssProperty("#display div[index='0']", "height", "50px")
+      .assert.cssProperty("#display div[index='0']", "background-color", "rgba(0, 0, 0, 0.5)")
+      .end();
+  },
 };
