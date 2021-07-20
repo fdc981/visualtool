99ed57cd90184d032ea146b64c2798d5a6b9d1cc
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-07-20 20:34:31 +0930

    Added tests for opening editing windowA
    Two e2e tests have been added in this commit. They check whether the editing window for a shape can be opened through the context menu, or holding shift and right clicking.

--- tests/frontend.js
+++ tests/frontend.js
@@ -39,4 +39,43 @@ module.exports = {
             .assert.cssProperty("#display div[index='0']", "background-color", "rgba(0, 0, 0, 0.5)")
             .end();
     },
+
+    "An edit window can be opened by holding shift and right-clicking on a shape" : function(browser) {
+        browser
+            .url('http://localhost:8080')
+            .waitForElementVisible('#display')
+            .moveToElement("#display", 50, 50)
+            .mouseButtonClick("right")
+            .waitForElementVisible('#context-menu')
+        /* create a shape */
+            .useXpath()
+            .click("//a[normalize-space()='add shape']")
+            .useCss()
+        /* edit it */
+            .waitForElementVisible("#display div[index='0']")
+            .keys('') // press and hold shift
+            .click("#display div[index='0']")
+            .assert.visible("#display .edit-region")
+    },
+
+    "An edit window can be opened via the 'edit shape' button of the context menu" : function(browser) {
+        browser
+            .url('http://localhost:8080')
+            .waitForElementVisible('#display')
+            .moveToElement("#display", 50, 50)
+            .mouseButtonClick("right")
+            .waitForElementVisible('#context-menu')
+        /* create a shape */
+            .useXpath()
+            .click("//a[normalize-space()='add shape']")
+            .useCss()
+        /* edit it */
+            .waitForElementVisible("#display div[index='0']")
+            .moveToElement("#display div[index='0']", 0, 0)
+            .mouseButtonClick("right")
+            .useXpath()
+            .click("//a[normalize-space()='edit shape']")
+            .useCss()
+            .assert.visible("#display .edit-region")
+    },
 };
