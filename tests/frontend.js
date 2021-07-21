2123b9d11bcdf07488f466478e67ef23d10eced0
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-07-21 16:02:22 +0930

    Added e2e tests for context menu
    Three tests on various expected behaviours of the context menu are included here. The last one, namely "Context menu disappears after being clicked on", currently fails.

--- tests/frontend.js
+++ tests/frontend.js
@@ -56,6 +56,7 @@ module.exports = {
             .keys('') // press and hold shift
             .click("#display div[index='0']")
             .assert.visible("#display .edit-region")
+            .end()
     },
 
     "An edit window can be opened via the 'edit shape' button of the context menu" : function(browser) {
@@ -77,5 +78,60 @@ module.exports = {
             .click("//a[normalize-space()='edit shape']")
             .useCss()
             .assert.visible("#display .edit-region")
+            .end()
+    },
+
+    "Context menu can be opened on the viewport with expected items" : function(browser) {
+        browser
+            .url('http://localhost:8080')
+            .waitForElementVisible('#display')
+            .moveToElement("#display", 50, 50)
+            .mouseButtonClick("right")
+            .waitForElementVisible('#context-menu')
+        /* expected elements */
+            .useXpath()
+            .assert.visible("//ul[@id = 'context-menu']")
+            .assert.visible("//ul[@id = 'context-menu']//a[normalize-space()='add shape']")
+            .assert.visible("//ul[@id = 'context-menu']//a[normalize-space()='save workspace']")
+            .useCss()
+            .end()
+    },
+
+    "Context menu can be opened on a shape with expected items" : function(browser) {
+        browser
+            .url('http://localhost:8080')
+            .waitForElementVisible('#display')
+            .moveToElement("#display", 50, 50)
+            .mouseButtonClick("right")
+            .waitForElementVisible('#context-menu')
+        /* add shape */
+            .useXpath()
+            .click("//a[normalize-space()='add shape']")
+            .useCss()
+            .waitForElementVisible("#display div[index='0']")
+            .moveToElement("#display div[index='0']", 0, 0)
+            .mouseButtonClick("right")
+        /* check expected elements */
+            .useXpath()
+            .assert.visible("//ul[@id = 'context-menu']")
+            .assert.visible("//ul[@id = 'context-menu']//a[normalize-space()='edit shape']")
+            .assert.visible("//ul[@id = 'context-menu']//a[normalize-space()='delete shape']")
+            .assert.visible("//ul[@id = 'context-menu']//a[normalize-space()='copy shape']")
+            .useCss()
+            .end()
+    },
+
+    "Context menu disappears after being clicked on" : function(browser) {
+        browser
+            .url('http://localhost:8080')
+            .waitForElementVisible('#display')
+            .moveToElement("#display", 50, 50)
+            .mouseButtonClick("right")
+            .waitForElementVisible('#context-menu')
+            .useXpath()
+            .click("//a[normalize-space()='add shape']")
+            .useCss()
+            .assert.cssProperty("ul#context-menu", "display", "none")
+            .end()
     },
 };
