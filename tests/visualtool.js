bcf2e87c299d4eaed7250782b79591c15c3fd79d
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-07-29 16:49:56 +0930

    Fix indentation for code of page object
    The `tests/visualtool.js` page object contains indentation of size 2 - this commit changes that indentation size to 4.

--- tests/visualtool.js
+++ tests/visualtool.js
@@ -1,107 +1,107 @@
 
 var toolbarElements = {
-  toolbar: {
-    selector: '#toolbar',
-    locateStrategy: 'css selector'
-  },
-  toolbarAddShape: {
-    selector: "//div[@id = 'toolbar']//button[normalize-space()='add shape']",
-    locateStrategy: 'xpath'
-  }
+    toolbar: {
+        selector: '#toolbar',
+        locateStrategy: 'css selector'
+    },
+    toolbarAddShape: {
+        selector: "//div[@id = 'toolbar']//button[normalize-space()='add shape']",
+        locateStrategy: 'xpath'
+    }
 };
 
 var displayElements = {
-  display: {
-    selector: "#display",
-    locateStrategy: "css selector"
-  },
-  firstShape: {
-    selector: "#display div[index='0']",
-    locateStrategy: "css selector"
-  },
-  secondShape: {
-    selector: "#display div[index='1']",
-    locateStrategy: "css selector"
-  },
-  thirdShape: {
-    selector: "#display div[index='2']",
-    locateStrategy: "css selector"
-  },
+    display: {
+        selector: "#display",
+        locateStrategy: "css selector"
+    },
+    firstShape: {
+        selector: "#display div[index='0']",
+        locateStrategy: "css selector"
+    },
+    secondShape: {
+        selector: "#display div[index='1']",
+        locateStrategy: "css selector"
+    },
+    thirdShape: {
+        selector: "#display div[index='2']",
+        locateStrategy: "css selector"
+    },
 };
 
 var contextMenuElements = {
-  contextMenu: {
-    selector: 'ul#context-menu',
-    locateStrategy: 'css selector'
-  },
+    contextMenu: {
+        selector: 'ul#context-menu',
+        locateStrategy: 'css selector'
+    },
 
-  menuAddShape: {
-    selector: "//ul[@id = 'context-menu']//a[normalize-space()='add shape']",
-    locateStrategy: 'xpath'
-  },
-  menuEditShape: {
-    selector: "//ul[@id = 'context-menu']//a[normalize-space()='edit shape']",
-    locateStrategy: 'xpath'
-  },
-  menuDeleteShape: {
-    selector: "//ul[@id = 'context-menu']//a[normalize-space()='delete shape']",
-    locateStrategy: 'xpath'
-  },
-  menuCopyShape: {
-    selector: "//ul[@id = 'context-menu']//a[normalize-space()='copy shape']",
-    locateStrategy: 'xpath'
-  },
-  menuSaveWorkspace: {
-    selector: "//ul[@id = 'context-menu']//a[normalize-space()='save workspace']",
-    locateStrategy: 'xpath'
-  },
+    menuAddShape: {
+        selector: "//ul[@id = 'context-menu']//a[normalize-space()='add shape']",
+        locateStrategy: 'xpath'
+    },
+    menuEditShape: {
+        selector: "//ul[@id = 'context-menu']//a[normalize-space()='edit shape']",
+        locateStrategy: 'xpath'
+    },
+    menuDeleteShape: {
+        selector: "//ul[@id = 'context-menu']//a[normalize-space()='delete shape']",
+        locateStrategy: 'xpath'
+    },
+    menuCopyShape: {
+        selector: "//ul[@id = 'context-menu']//a[normalize-space()='copy shape']",
+        locateStrategy: 'xpath'
+    },
+    menuSaveWorkspace: {
+        selector: "//ul[@id = 'context-menu']//a[normalize-space()='save workspace']",
+        locateStrategy: 'xpath'
+    },
 };
 
 
 var shapeListElements = {
-  shapeList: {
-    selector: '#shape-list',
-    locateStrategy: 'css selector'
-  },
+    shapeList: {
+        selector: '#shape-list',
+        locateStrategy: 'css selector'
+    },
 };
 
 
 module.exports = {
-  url: "http://localhost:8080",
+    url: "http://localhost:8080",
 
-  elements: [
-    toolbarElements,
-    displayElements,
-    contextMenuElements,
-    shapeListElements,
-  ],
+    elements: [
+        toolbarElements,
+        displayElements,
+        contextMenuElements,
+        shapeListElements,
+    ],
 
-  commands: [{
-    openMenu() {
-      this.api.mouseButtonClick("right");
+    commands: [{
+        openMenu() {
+            this.api.mouseButtonClick("right");
 
-      return this.waitForElementVisible('@contextMenu');
-    },
+            return this.waitForElementVisible('@contextMenu');
+        },
 
-    shiftClick(targetSelector) {
-      this.api.keys(''); // press and hold shift
-      
-      return this.click(targetSelector);
-    },
+        shiftClick(targetSelector) {
+            this.api.keys(''); // press and hold shift
 
-    dragAndDrop(targetSelector, x, y) {
-      // drag and drop does not work??
-      let page = this;
+            return this.click(targetSelector);
+        },
 
-      this.moveToElement(targetSelector, 0, 0, function(val, visualtool) {
-        page.api.mouseButtonDown("left", function(val, visualtool) {
-          page.api.moveToElement("body", x, y, function(val, visualtool) {
-            page.api.mouseButtonUp("left");
-          });
-        });
-      });
+        dragAndDrop(targetSelector, x, y) {
+            // drag and drop does not work??
+            let page = this;
 
-      return this;
-    }
-  }]
+            this.moveToElement(targetSelector, 0, 0, function(val, visualtool) {
+                page.api.mouseButtonDown("left", function(val, visualtool) {
+                    page.api.moveToElement("body", x, y, function(val, visualtool) {
+                        page.api.mouseButtonUp("left");
+                    });
+                });
+            });
+
+            return this;
+        }
+    }]
 };
