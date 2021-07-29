5c7d8c043868c59d72023d90ee2847a876e4fd56
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-07-29 23:28:04 +0930

    Separate shape and context menu tests
    The end-to-end tests within `frontend.js` included tests which checked for specific functionality between the shape and the context menu. The shape tests were unrelated to the context menu tests, so in this commit, each test has been moved into either one of the two test suites:
    
    - `shape.js` contains end-to-end tests on shape rendering and shape manipulation, and
    - `context-menu.js` contains end-to-end tests on expected features of the context menu.

---
+++ tests/context-menu.js
@@ -0,0 +1,47 @@
+/*
+ * Tests on the custom context menu of visualtool.
+ */
+
+module.exports = {
+    "Context menu can be opened on the viewport with expected items" : function(browser) {
+        let visualtool = browser.page.visualtool();
+
+        visualtool.navigate()
+                  .waitForElementVisible('@display')
+                  .moveToElement("@display", 50, 50)
+                  .openMenu()
+                  .assert.visible("@menuAddShape")
+                  .assert.visible("@menuSaveWorkspace")
+                  .assert.visible("@menuOpenCollection")
+                  .end()
+    },
+
+    "Context menu can be opened on a shape with expected items" : function(browser) {
+        let visualtool = browser.page.visualtool();
+
+        visualtool.navigate()
+                  .waitForElementVisible('@display')
+                  .moveToElement("@display", 50, 50)
+                  .openMenu()
+                  .click("@menuAddShape")
+                  .waitForElementVisible("@firstShape")
+                  .moveToElement("@firstShape", 0, 0)
+                  .openMenu()
+                  .assert.visible("@menuEditShape")
+                  .assert.visible("@menuDeleteShape")
+                  .assert.visible("@menuCopyShape")
+                  .end()
+    },
+
+    "Context menu disappears after clicking on one of its items" : function(browser) {
+        let visualtool = browser.page.visualtool();
+
+        visualtool.navigate()
+                  .waitForElementVisible("@display")
+                  .moveToElement("@display", 50, 50)
+                  .openMenu()
+                  .click("@menuAddShape")
+                  .assert.cssProperty("@contextMenu", "display", "none")
+                  .end()
+    },
+};
--- tests/shape.js
+++ tests/shape.js
@@ -1,3 +1,6 @@
+/*
+ * Tests on shape manipulation and rendering.
+ */
 
 module.exports = {
     "New shape can be created via 'add shape' button of toolbar" : function(browser) {
@@ -63,19 +66,7 @@ module.exports = {
                   .end()
     },
 
-    "Context menu can be opened on the viewport with expected items" : function(browser) {
-        let visualtool = browser.page.visualtool();
-
-        visualtool.navigate()
-                  .waitForElementVisible('@display')
-                  .moveToElement("@display", 50, 50)
-                  .openMenu()
-                  .assert.visible("@menuAddShape")
-                  .assert.visible("@menuSaveWorkspace")
-                  .end()
-    },
-
-    "Context menu can be opened on a shape with expected items" : function(browser) {
+    "Saving workspace (through context menu) preserves shapes after reloading page" : function(browser) {
         let visualtool = browser.page.visualtool();
 
         visualtool.navigate()
@@ -83,25 +74,18 @@ module.exports = {
                   .moveToElement("@display", 50, 50)
                   .openMenu()
                   .click("@menuAddShape")
-                  .waitForElementVisible("@firstShape")
-                  .moveToElement("@firstShape", 0, 0)
+                  .assert.visible("@firstShape")
                   .openMenu()
-                  .assert.visible("@menuEditShape")
-                  .assert.visible("@menuDeleteShape")
-                  .assert.visible("@menuCopyShape")
-                  .end()
-    },
+                  .click("@menuSaveWorkspace")
+                  .api.refresh()
 
-    "Context menu disappears after clicking on one of its items" : function(browser) {
-        let visualtool = browser.page.visualtool();
-
-        visualtool.navigate()
-                  .waitForElementVisible("@display")
-                  .moveToElement("@display", 50, 50)
-                  .openMenu()
-                  .click("@menuAddShape")
-                  .assert.cssProperty("@contextMenu", "display", "none")
-                  .end()
+        visualtool.waitForElementVisible('@display')
+                  .assert.visible("@firstShape")
+                  .assert.cssProperty("@firstShape", "position", "absolute")
+                  .assert.cssProperty("@firstShape", "width", "50px")
+                  .assert.cssProperty("@firstShape", "height", "50px")
+                  .assert.cssProperty("@firstShape", "background-color", "rgba(0, 0, 0, 0.5)")
+                  .end();
     },
 
     "Shapes can be dragged and dropped into any point on the screen" : function(browser) {
@@ -125,27 +109,5 @@ module.exports = {
                       .assert.cssProperty("@firstShape", "left", expectedLeft)
                       .assert.cssProperty("@firstShape", "top", expectedTop)
         });
-    },
-
-    "Saving workspace (through context menu) preserves shapes after reloading page" : function(browser) {
-        let visualtool = browser.page.visualtool();
-
-        visualtool.navigate()
-                  .waitForElementVisible('@display')
-                  .moveToElement("@display", 50, 50)
-                  .openMenu()
-                  .click("@menuAddShape")
-                  .assert.visible("@firstShape")
-                  .openMenu()
-                  .click("@menuSaveWorkspace")
-                  .api.refresh()
-
-        visualtool.waitForElementVisible('@display')
-                  .assert.visible("@firstShape")
-                  .assert.cssProperty("@firstShape", "position", "absolute")
-                  .assert.cssProperty("@firstShape", "width", "50px")
-                  .assert.cssProperty("@firstShape", "height", "50px")
-                  .assert.cssProperty("@firstShape", "background-color", "rgba(0, 0, 0, 0.5)")
-                  .end();
     },
 };
