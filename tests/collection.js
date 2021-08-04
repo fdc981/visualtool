03f87b310c77037afbc7af235d64d7033ab360f4
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-08-04 21:35:31 +0930

    Add e2e test for Collection
    The new e2e test checks whether an opened Collection can be dragged and dropped to any part of the screen.

--- tests/collection.js
+++ tests/collection.js
@@ -42,4 +42,28 @@ module.exports = {
                   .assert.visible("#collection [index='0']")
                   .end();
     },
+
+    "The collection can be dragged and dropped to any point on the screen" : function(browser) {
+        let visualtool = browser.page.visualtool();
+
+        visualtool.navigate().waitForElementVisible("@display");
+
+        visualtool.api.getElementSize("#display", function (size) {
+            let randomX = Math.floor(Math.random() * size.value.width);
+            let randomY = Math.floor(Math.random() * size.value.height);
+            let expectedLeft = randomX + "px";
+            let expectedTop = randomY + "px";
+
+            console.log(`Initialised random point (${randomX}, ${randomY})`);
+
+            visualtool.openMenu()
+                      .click("@menuOpenCollection")
+                      .waitForElementVisible("@collection")
+                      .api.keys(''); // press and hold control
+            visualtool.dragAndDrop("@collection", randomX, randomY)
+                      .assert.cssProperty("@collection", "left", expectedLeft)
+                      .assert.cssProperty("@collection", "top", expectedTop)
+                      .end();
+        });
+    },
 };
