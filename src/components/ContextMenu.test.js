4d9d847bb45687dcf060d9e2dedc4d5f82b69c41
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-09-11 13:07:34 +0930

    Add provision of shapeList
    The ContextMenu.test.js test suite now provides a mock `shapeList` prop, so that the test suite now passes with no errors.

--- src/components/ContextMenu.test.js
+++ src/components/ContextMenu.test.js
@@ -1,5 +1,11 @@
 import { mount } from '@vue/test-utils'
 import ContextMenu from './ContextMenu.vue'
+import { config } from '@vue/test-utils'
+
+// provide a mock shapeList prop
+config.provide['shapeList'] = {
+  val: {}
+}
 
 test('displays message', () => {
   const menu = mount(ContextMenu, {
