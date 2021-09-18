5f75acaa2d708b3f243ef139d03b7835e6718c6e
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-09-18 21:28:34 +0930

    Install babel-plugin-transform-generator and regenerator-runtime
    These two packages should allow babel to transpile async functions, which will be needed for unit testing with events.
    
    An appropriate import statement for regenerator-runtime has also been added to ContextMenu.test.js.

--- src/components/ContextMenu.test.js
+++ src/components/ContextMenu.test.js
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils'
 import ContextMenu from './ContextMenu.vue'
 import ContextMenuItem from './ContextMenuItem.vue'
 import { config } from '@vue/test-utils'
+import "regenerator-runtime/runtime";
 
 // provide a mock shapeList prop
 config.provide['shapeList'] = {
