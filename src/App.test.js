8c9f512ff208fb95cbed4a700b2e3b20cc3d291e
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-11-06 11:48:54 +1030

    Add unit tests for App
    This commit adds the unit tests for the App component.

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import { mount } from '@vue/test-utils'
+import App from './App.vue'
+import ContextMenu from './components/ContextMenu.vue'
+import Collection from './components/Collection.vue'
+import { config } from '@vue/test-utils'
+import "regenerator-runtime/runtime";
+
+test("mounts with no error", () => {
+    const wrapper = mount(App);
+});
+
+test("contains necessary components", () => {
+    const wrapper = mount(App);
+
+    expect(wrapper.find("div#toolbar").exists()).toBe(true);
+    expect(wrapper.findComponent(ContextMenu).exists()).toBe(true);
+    expect(wrapper.find("div#display").exists()).toBe(true);
+    expect(wrapper.find("div#shape-list").exists()).toBe(true);
+    expect(wrapper.findComponent(Collection).exists()).toBe(true);
+});
