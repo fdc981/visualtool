40ac3ef0e3d96da73ff6e004e8045a4c84cb063f
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-12-29 21:19:20 +1030

    Export vuex store mutations
    In this commit, the mutations of the vuex store are exported so that they can be unit tested in an external module.

--- store/index.js
+++ store/index.js
@@ -25,6 +25,47 @@ if (shapeList !== null) {
     currID = shapeList.map(x => Number(x.id)).reduce(reducer, 0) + 1;
 }
 
+export const mutations = {
+    addShape(state) {
+        state.shapeList.push({
+            id: state.currID,
+            name: "Unnamed " + state.currID,
+            style: "position: absolute; width: 50px; height: 50px; background-color: rgba(0,0,0,0.5);",
+            currentlyEditing: false
+        });
+
+        state.currID += 1;
+    },
+
+    deleteShape(state, targetShapeNode) {
+        let ind = Number(targetShapeNode.attributes.index.value);
+
+        state.shapeList = state.shapeList.filter((element, index) => {
+            return index !== ind;
+        });
+    },
+
+    copyShape(state, targetShapeNode) {
+        // Copies a shape, assigning it with currID
+        let ind = Number(targetShapeNode.attributes.index.value);
+        let newShape = JSON.parse(JSON.stringify(state.shapeList[ind]));
+        newShape.id = state.currID;
+
+        state.shapeList.push(newShape);
+
+        state.currID += 1;
+    },
+
+    editShape(state, targetShapeNode) {
+        let shape = state.shapeList[Number(targetShapeNode.attributes.index.value)];
+        shape.currentlyEditing = true;
+    },
+
+    saveShapeList(state) {
+        localStorage.setItem("shapeList", JSON.stringify(state.shapeList));
+    },
+};
+
 export let store = new Vuex.Store({
     state: {
         shapeList: shapeList,
@@ -32,44 +73,5 @@ export let store = new Vuex.Store({
         held: null
     },
 
-    mutations: {
-        addShape(state) {
-            state.shapeList.push({
-                id: state.currID,
-                name: "Unnamed " + state.currID,
-                style: "position: absolute; width: 50px; height: 50px; background-color: rgba(0,0,0,0.5);",
-                currentlyEditing: false
-            });
-
-            state.currID += 1;
-        },
-
-        deleteShape(state, targetShapeNode) {
-            let ind = Number(targetShapeNode.attributes.index.value);
-
-            state.shapeList = state.shapeList.filter((element, index) => {
-                return index !== ind;
-            });
-        },
-
-        copyShape(state, targetShapeNode) {
-            // Copies a shape, assigning it with currID
-            let ind = Number(targetShapeNode.attributes.index.value);
-            let newShape = JSON.parse(JSON.stringify(state.shapeList[ind]));
-            newShape.id = state.currID;
-
-            state.shapeList.push(newShape);
-
-            state.currID += 1;
-        },
-
-        editShape(state, targetShapeNode) {
-            let shape = state.shapeList[Number(targetShapeNode.attributes.index.value)];
-            shape.currentlyEditing = true;
-        },
-
-        saveShapeList(state) {
-            localStorage.setItem("shapeList", JSON.stringify(state.shapeList));
-        },
-    }
+    mutations: mutations
 });
