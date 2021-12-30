08fc2ceb5ca5ec67fb5cf6728393692d58b1010c
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-12-30 15:39:56 +1030

    Edit Collection tests for vuex integration
    The Collection is a component that uses the vuex store. This commit makes several changes to Collection.test.js:
    
    - `describe` and `it` are now used to specify the tests
    - accordingly, all the test case names have been changed to be grammatical
    - the entire Vuex store is initialised before each test and passed to mount() along with localVue

--- src/components/Collection.test.js
+++ src/components/Collection.test.js
@@ -1,132 +1,141 @@
-import { mount } from '@vue/test-utils'
+import { config, createLocalVue, mount } from '@vue/test-utils'
 import Collection from './Collection.vue'
 import ShapeRender from './ShapeRender.vue'
-import { config } from '@vue/test-utils'
 import { makeShapeList } from "../utils.js";
+import { mutations } from "../../store/index.js";
 import "regenerator-runtime/runtime";
+import Vuex from 'vuex';
 
-config.provide['shapeHeld'] = {
-    val : null
-};
+const localVue = createLocalVue();
 
-config.provide['shapeList'] = [];
+localVue.use(Vuex);
 
-test("is invisible by default", () => {
-    const wrapper = mount(Collection);
+describe("Collection.vue", () => {
+    const numShapes = 5;
 
-    expect(wrapper.find('div').isVisible()).toBe(false);
-});
+    let store;
 
-test("is invisible if collectionVisible is false", () => {
-    const wrapper = mount(Collection, {
-        propsData: {
-            collectionVisible: false
-        }
+    beforeEach(() => {
+        store = new Vuex.Store({
+            state: { shapeList: makeShapeList(numShapes), held: null },
+            mutations: mutations
+        });
     });
 
-    expect(wrapper.find('div').isVisible()).toBe(false);
-});
+    it("is invisible by default", () => {
+        const wrapper = mount(Collection);
 
-test("is visible if collectionVisible is true", () => {
-    const wrapper = mount(Collection, {
-        propsData: {
-            collectionVisible: true
-        }
+        expect(wrapper.find('div').isVisible()).toBe(false);
     });
 
-    expect(wrapper.find('div').isVisible()).toBe(true);
-});
-
-test("when visible, it is ctrl+draggable", async () => {
-    const wrapper = mount(Collection, {
-        propsData: {
-            collectionVisible: true
-        }
-    });
-
-    await wrapper.trigger("mousedown", {
-        ctrlKey: true
-    });
-
-    // select a random position to move the mouse to:
-    let randomX = Math.floor(Math.random() * window.innerWidth);
-    let randomY = Math.floor(Math.random() * window.innerHeight);
-
-    await document.dispatchEvent(new MouseEvent("mousemove", {
-        clientX: randomX,
-        clientY: randomY
-    }));
-
-    await document.dispatchEvent(new MouseEvent("mouseup"));
-
-    expect(wrapper.element.style.left).toBe(randomX / window.innerWidth * 100 + "%");
-    expect(wrapper.element.style.top).toBe(randomY / window.innerHeight * 100 + "%");
-});
-
-test("a visible ShapeRender can be dragged and dropped into a visible Collection, adding it", async () => {
-    const CollectionWrapper = mount(Collection, {
-        propsData: {
-            collectionVisible: true
-        }
-    });
-
-    const ShapeRenderWrapper = mount(ShapeRender, {
-        propsData: {
-            shapeIndex: 0
-        },
-
-        provide: {
-            shapeList: makeShapeList(1)
-        }
-    });
-
-    let oldStyle = ShapeRenderWrapper.element.style.cssText;
-
-    await ShapeRenderWrapper.trigger("dragstart", {
-        ctrlKey: true
-    });
-
-    let style = CollectionWrapper.element.style;
-    let midX = style.left + style.width / 2;
-    let midY = style.top + style.height / 2;
-
-    await document.dispatchEvent(new MouseEvent("drag", {
-        clientX: midX,
-        clientY: midY
-    }));
-
-    await CollectionWrapper.trigger("drop");
-
-    let currentStyle = ShapeRenderWrapper.element.style.cssText;
-
-    expect(oldStyle).toBe(currentStyle);
-    expect(CollectionWrapper.find("div[index='0'][class='shape']").style.cssText).toBe(currentStyle);
-});
-
-test("dragging a shape out of it adds it to the shapeList", async () => {
-    const CollectionWrapper = mount(Collection, {
-        propsData: {
-            collectionVisible: true
-        },
-
-        data() {
-            return {
-                collection: [
-                    {
-                        type: "shape",
-                        name : "Unnamed " + 0,
-                        style : "position: absolute; width: 50px; height: 50px; background-color: rgba(0,0,0,0.5);",
-                    }
-                ]
+    it("is invisible if collectionVisible is false", () => {
+        const wrapper = mount(Collection, {
+            propsData: {
+                collectionVisible: false
             }
-        }
+        });
+
+        expect(wrapper.find('div').isVisible()).toBe(false);
     });
 
-    await CollectionWrapper.find("div[index=0]").trigger("dragstart");
+    it("is visible if collectionVisible is true", () => {
+        const wrapper = mount(Collection, {
+            propsData: {
+                collectionVisible: true
+            }
+        });
 
-    await document.dispatchEvent("drag");
+        expect(wrapper.find('div').isVisible()).toBe(true);
+    });
 
-    await document.dispatchEvent("drop");
+    it("is ctrl+draggable when it is visible", async () => {
+        const wrapper = mount(Collection, {
+            propsData: { collectionVisible: true },
+            store,
+            localVue
+        });
 
-    // expect a ShapeRender to be present in the shapeList
+        await wrapper.trigger("mousedown", {
+            ctrlKey: true
+        });
+
+        // select a random position to move the mouse to:
+        let randomX = Math.floor(Math.random() * window.innerWidth);
+        let randomY = Math.floor(Math.random() * window.innerHeight);
+
+        await document.dispatchEvent(new MouseEvent("mousemove", {
+            clientX: randomX,
+            clientY: randomY
+        }));
+
+        await document.dispatchEvent(new MouseEvent("mouseup"));
+
+        expect(wrapper.element.style.left).toBe(randomX / window.innerWidth * 100 + "%");
+        expect(wrapper.element.style.top).toBe(randomY / window.innerHeight * 100 + "%");
+    });
+
+    it("can collect shapes by dragging and dropping a ShapeRender into it", async () => {
+        const CollectionWrapper = mount(Collection, {
+            propsData: { collectionVisible: true },
+            store,
+            localVue
+        });
+
+        const ShapeRenderWrapper = mount(ShapeRender, {
+            propsData: { shapeIndex: 0 },
+            store,
+            localVue
+        });
+
+        let oldStyle = ShapeRenderWrapper.element.style.cssText;
+
+        await ShapeRenderWrapper.trigger("dragstart", {
+            ctrlKey: true
+        });
+
+        let style = CollectionWrapper.element.style;
+        let midX = style.left + style.width / 2;
+        let midY = style.top + style.height / 2;
+
+        await document.dispatchEvent(new MouseEvent("drag", {
+            clientX: midX,
+            clientY: midY
+        }));
+
+        await CollectionWrapper.trigger("drop");
+
+        let currentStyle = ShapeRenderWrapper.element.style.cssText;
+        let collectedShape = CollectionWrapper.find("div[index='0'][class='shape']").element;
+
+        expect(oldStyle).toBe(currentStyle);
+        expect(collectedShape.style.cssText).toBe(currentStyle);
+    });
+
+    it("can add collected shapes into the display by dragging a shape out of it", async () => {
+        const CollectionWrapper = mount(Collection, {
+            propsData: { collectionVisible: true },
+
+            data() {
+                return {
+                    collection: [
+                        {
+                            type: "shape",
+                            name : "Unnamed " + 0,
+                            style : "position: absolute; width: 50px; height: 50px; background-color: rgba(0,0,0,0.5);",
+                        }
+                    ]
+                }
+            },
+            store,
+            localVue
+        });
+
+        await CollectionWrapper.find("div[index=0]").trigger("dragstart");
+
+        await document.dispatchEvent("drag");
+
+        await document.dispatchEvent("drop");
+
+        // expect a ShapeRender to be present in the shapeList
+    });
 });
